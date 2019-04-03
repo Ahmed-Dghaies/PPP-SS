b5b1b2a2c2bfb2179d7732d712865a03d943f0f8
Madou <dghais.ahmed7@gmail.com>
2019-04-03 06:06:11 +0100

    updating index.js

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 
-  const express = require('express')
-  const bodyParser = require('body-parser')
-  const cors = require('cors')
-  const PORT = process.env.PORT || 3000
-  const api = require('./routes/api')
-  const app = express()
-  const path = require('path');
-  
-  
-  app.use(cors())
-  app.use(bodyParser.json())
-  app.use(express.static('./dist/App'));
-
-  app.get('/*', function(req,res) {
-    
-    res.sendFile(path.join(__dirname,'/dist/App/index.html'));
-    });
-
-  app.get('/', function(req, res) {
-      res.send('Hello from server')
-  }  )
-  
-  
-  app.use('/api',api)
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 3000
+const api = require('./routes/api')
+const app = express()
+const path = require('path');
 
 
-  app.listen (PORT, function(){
-      console.log('Server running at localhost: '+ PORT)
-  })
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+/*app.get('/', function (req, res) {
+    res.send('Hello from server')
+})*/
+
+
+app.use('/api', api)
+
+
+app.listen(PORT, function () {
+    console.log('Server running at localhost: ' + PORT)
+})
 
 
