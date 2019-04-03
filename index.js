d66a01c24eb1f62024753e3bb10b3100064155f8
Madou <dghais.ahmed7@gmail.com>
2019-04-03 04:56:01 +0100

    Updating index.js

--- index.js
+++ index.js
@@ -5,10 +5,12 @@
   const PORT = process.env.PORT || 3000
   const api = require('./routes/api')
   const app = express()
+  const path = require('path');
   
   
   app.use(cors())
   app.use(bodyParser.json())
+  app.use(express.static(__dirname + '/dist/App'));
   
   app.get('/', function(req, res) {
       res.send('Hello from server')
