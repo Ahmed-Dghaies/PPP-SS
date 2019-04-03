b03b95e15717f29325163a86d37b9bc983247b32
Madou <dghais.ahmed7@gmail.com>
2019-04-03 06:17:20 +0100

    updating package.json

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const path = require('path');
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, './dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/App/index.html'));
 });
 
 /*app.get('/', function (req, res) {
