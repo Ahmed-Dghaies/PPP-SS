6be97506007e77c7603acc1dbc49e6880451d9e1
gbo96 <gbenothmen2@gmail.com>
2019-04-16 13:23:16 +0100

    crud client

---
+++ models/Client.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+const clientSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    email: {type: String},
+    type: {type: String, required: true},
+    state: {type: String, required: true},
+    matFisc: {type: String, required: true},
+    phone: {type: Number, required: true},
+    plafondCredit: {type: Number, required: true},
+    address:{
+        city: {type: String, required: true},
+        postal: {type: Number, required: true}
+    },
+    fax: {type: Number, required: true},
+    region: {type: String, required: true}
+});
+
+
+module.exports = mongoose.model('client', clientSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,34 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-const mongoose = require('mongoose')
-const db = "mongodb+srv://Manager:22b7284ab75c8861dfcee4bbf3918848@ppp-ss-authentication-nqw72.mongodb.net/PPP-SS-Auth?retryWrites=true"
 const jwt = require('jsonwebtoken')
+const verifyToken = require('../middleware/check-auth');
 
-mongoose.connect(db ,{ useNewUrlParser: true }, err => {
-    if (err) {
-        console.error('error: ' + err)
-    }
-    else {
-        console.log('connection established')
-    }
-})
 
-function verifyToken(req, res, next) {
-    if (!req.headers.authorization) {
-        return res.status(401).send('Unothorized request')
-    }
-    let token = req.headers.authorization.split(' ')[1]
-    if (token === 'null') {
-        return res.status(401).send('Unothorized request')
-    }
-    let payload = jwt.verify(token,'secretKey')
-    if(!payload) {
-        return res.status(401).send('Unothorized request')
-    }
-    req.userId = payload.subject
-    next()
-}
+
 
 router.get('/', (req,res)=>{
     res.send('From API route')
