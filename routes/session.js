fcac470584c43deaa2efb1a6f37720a1cda23f1d
Ahmed-Dghaies <dghais.ahmed7@gmail.com>
2019-07-05 02:10:48 +0100

    fixing reference error

--- routes/session.js
+++ routes/session.js
@@ -2,7 +2,6 @@ const express = require('express');
 const verifyToken = require('../middleware/check-auth');
 const SessionModel = require('../models/Session');
 const pompisteModel = require('../models/Pompiste');
-const SessionModel = require('../models/Session');
 
 // setting router variable
 const router = express.Router();
