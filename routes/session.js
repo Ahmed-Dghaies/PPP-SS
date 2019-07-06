da2a8292c737d73b5d0e40405dc9020eb17404f9
Ahmed-Dghaies <dghais.ahmed7@gmail.com>
2019-07-06 12:13:18 +0100

    session edit

--- routes/session.js
+++ routes/session.js
@@ -151,4 +151,45 @@ router.get('/get-session-pompiste/', (req, res) => {
         });
 });
 
+// Update Session
+router.put('/update/:id', (req, res) => {
+
+    let id = req.params.id;
+    let session = req.body.session;
+
+    SessionModel.findOne({
+            _id: id
+        }).exec()
+        .then(doc => {
+            if (!doc) {
+                res.status(404).json({
+                    message: 'Session Introuvable'
+                });
+            } else {
+                SessionModel.updateOne({
+                        _id: id
+                    }, session).exec()
+                    .then(result => {
+                        session = {
+                            _id: id,
+                            ...session
+                        }
+                        res.status(200).json({
+                            citerne
+                        });
+                    })
+                    .catch(err => {
+                        res.status(500).json({
+                            erreur: err
+                        });
+                    });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                erreur: err
+            });
+        });
+});
+
 module.exports = router;
