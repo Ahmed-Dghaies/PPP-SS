2ae71dd58e48da47027c29cc8ee6e0ee5d0b1684
Ahmed-Dghaies <dghais.ahmed7@gmail.com>
2019-07-06 14:03:42 +0100

    session update

--- routes/session.js
+++ routes/session.js
@@ -22,8 +22,9 @@ router.put('/closeSession', verifyToken, (req, res) => {
 // Update Session
 router.put('/update/:id', verifyToken, function (req, res) {
     let id = req.params.id;
-    let description = req.body;
-    SessionModel.updateOne({ _id: id }, { $set: { description: 'description' } }).then(result => {
+    let description = req.body.description;
+    console.log(description);
+    SessionModel.updateOne({ _id: id }, { $set: { description: description } }).then(result => {
         res.status(200).json({
             result
         });
