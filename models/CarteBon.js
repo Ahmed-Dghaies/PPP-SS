b800d3019ea7df1ed1c76b06a74972dd964eccb9
Ahmed-Dghaies <dghais.ahmed7@gmail.com>
2019-07-16 12:58:49 +0100

    carte bon model change (total Value)

--- models/CarteBon.js
+++ models/CarteBon.js
@@ -5,6 +5,7 @@ const carteBon = Schema({
     cardType: { type: String, required: true },
     cardValue: { type: Number, required: true },
     numberOfCards: { type: Number, required: true},
+    totalValue: { type: Number, required: true},
     date: { type: String, required: true }
 });
 
