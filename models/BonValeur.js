fb1e9ab5685c3ceb1569a91deead4dd5f61a3061
Ahmed-Dghaies <dghais.ahmed7@gmail.com>
2019-07-17 02:43:56 +0100

    modifying bon valeur

--- models/BonValeur.js
+++ models/BonValeur.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bonValeur = Schema({
-    cardNumber: { type: String, required: true },
-    clientCode: { type: String, required: true },
-    clientName: { type: String, required: true },
-    clientAdress: { type: String, required: true },
     cardValue: { type: Number, required: true },
+    numberOfCards: { type: Number, required: true },
+    totalValue: { type: Number, required: true },
     date: { type: String, required: true }
 });
 
