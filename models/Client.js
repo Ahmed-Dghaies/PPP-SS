e42d049a36579a76c4e22a665ff81fa0d1ab564e
Ahmed-Dghaies <dghais.ahmed7@gmail.com>
2019-07-18 04:53:53 +0100

    client modification

--- models/Client.js
+++ models/Client.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 
 const clientSchema = mongoose.Schema({
     name: { type: String, required: true },
-    number: { type: Number, required: true},
-    email: {type: String},
-    type: {type: String, required: true},
-    state: {type: String, required: true},
-    matFisc: {type: String, required: true},
-    phone: {type: Number, required: true},
-    plafondCredit: {type: Number, required: true},
-    address:{
-        city: {type: String, required: true},
-        postal: {type: Number, required: true}
+    number: { type: String, required: true },
+    email: { type: String },
+    type: { type: String, required: true },
+    state: { type: String, required: true },
+    matFisc: { type: String, required: true },
+    phone: { type: Number, required: true },
+    plafondCredit: { type: Number, required: true },
+    address: {
+        city: { type: String, required: true },
+        postal: { type: Number, required: true }
     },
-    fax: {type: Number, required: true},
-    region: {type: String, required: true}
+    fax: { type: Number, required: true },
+    region: { type: String, required: true }
 });
 
 
