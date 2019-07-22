c3133dd269bb294224e2f0fc43c408e91a28d416
Ahmed-Dghaies <dghais.ahmed7@gmail.com>
2019-07-22 19:28:26 +0100

    adding session Id and card Number to steg et autres bon

--- models/StegEtAutre.js
+++ models/StegEtAutre.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const stegEtAutre = Schema({
+    cardNumber: { type: String, required: true },
     clientNumber: { type: String, required: true },
     clientName: { type: String, required: true },
     chauffeur: { type: String, required: true },
@@ -11,6 +12,7 @@ const stegEtAutre = Schema({
     numberOfCards: { type: Number, required: true },
     cardValue: { type: Number, required: true },
     totalValue: { type: Number, required: true },
+    sessionId: { type: String, required: true },
     date: { type: String, required: true }
 });
 
