82a17cc5a8f8e92bcceaf3af235a74c750b92b17
Ahmed-Dghaies <dghais.ahmed7@gmail.com>
2019-07-26 02:27:50 +0100

    especes model change

--- models/Espece.js
+++ models/Espece.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const espece = Schema({
     type: { type: Number, required: true },
     quantity: { type: Number, required: true },
+    totalValue: { type: Number, required: true },
+    sessionId: { type: String, required: true },
     state: { type: String, required: true }
 });
 
